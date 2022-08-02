d421627bb8d5d4e8b9aee7159d9efa737a1f1cb5
Dan Romanov <chcvlr@protonmail.com>
2022-08-02 06:20:36 +0200

    1

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('cheatsheet')
+const arr = [2,7,11,15]
+var twoSum = (nums,target)=>{
+    let m=new Map();
+ for(let i=0;i<nums.length;i++){
+     let n=nums[i];
+     let s=target-n;
+     if(m.has(s)){
+         return [i,m.get(s)]}m.set(n,i)
+ }
+}
+
+console.log(twoSum(arr, 9))
